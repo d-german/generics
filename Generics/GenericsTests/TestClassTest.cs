a5f9950d594664bae6fd5afde4d069dd5a635213
Damon German <damon.german@hyland.com>
2020-10-24 14:27:54 -0500

    made some changes

---
+++ Generics/GenericsTests/TestClassTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace GenericsTests
+{
+    public class Test
+    {
+        private int _value;
+
+        public void SetValue(int value)
+        {
+            _value = value;
+        }
+
+        public int GetValue()
+        {
+            int ret = _value;
+            return ret;
+        }
+    }
+
+    [TestFixture]
+    public class TestClassTest
+    {
+        [Test]
+        public void Test()
+        {
+            var test = new Test();
+            test.SetValue(5);
+
+            Assert.AreEqual(5, test.GetValue());
+        }
+    }
+}
