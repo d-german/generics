0bfe087bf518c4fc42c5ba4ba60fc3902f663667
Damon German <damon.german@hyland.com>
2020-10-26 12:43:20 -0500

    made some changes

--- Generics/GenericsTests/GenericMethodsTest.cs
+++ Generics/GenericsTests/GenericMethodsTest.cs
@@ -5,17 +5,25 @@ namespace GenericsTests
     [TestFixture]
     public class GenericMethodsTest
     {
-        private static void Swap<T>(ref T a, ref T b)
-        {
-            var temp = a;
-            a = b;
-            b = temp;
-        }
-
+        /// <summary>
+        /// Follows the OCP
+        /// Is an example of dynamic or runtime polymorphism
+        /// </summary>
         private static void Swap(ref object a, ref object b)
         {
             object temp = a;
             //var i = (int)a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// Follows the OCP
+        /// Is an example of static or compile polymorphism
+        /// </summary>
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
             a = b;
             b = temp;
         }
